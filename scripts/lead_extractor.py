5832f0c1778bb1c2e15f23a921d27dbd7a6b22c8
Brandon Wright <bwright2810@gmail.com>
2021-10-03 01:52:21 -0500

    note

--- scripts/lead_extractor.py
+++ scripts/lead_extractor.py
@@ -4,4 +4,4 @@ def run(*args):
   date = '10/2/2021'
   leads: list[Lead] = HAScraper.scrape(date)
   print(leads)
-  
+  # this is where we would pass the leads into another class that would save to DB
