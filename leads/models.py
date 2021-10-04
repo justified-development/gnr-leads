14104728f8fd90b3de90e56975519d3b4f8aeec1
Scott Wright <scottwright.qrf@gmail.com>
2021-10-03 21:43:59 -0500

    created save to DB class and model

--- leads/models.py
+++ leads/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Leads(models.Model):
-    lead_name = models.CharField(max_length=100)
+    user_name = models.CharField(max_length=100)
     first_call_time = models.IntegerField(max_length= 5)
-    total_call_time = models.IntegerField(max_length= 5)
-    time_created = models.DateTimeField()
+    total_talk_time = models.IntegerField(max_length= 5)
+    date = models.DateTimeField()
--- leads/services/save_to_DB.py
+++ leads/services/save_to_DB.py
@@ -1,17 +1,27 @@
-from models import Leads
+from HAScraper import Lead
 
 
-class Save_DB:
-    def __init__(self):
-        lead_name = Leads.objects.lead_name.all()
-        first_call_time = Leads.objects.first_call_time.all()
-        total_talk_time = Leads.objects.total_talk_time.all()
+class Save_DB(Lead):
+    def __init__(self, user, name, date, first_call_time, total_talk_time):
+        self.user = user
+        self.name = name
+        self.date = date
+        self.first_call_time = first_call_time
+        self.total_talk_time = total_talk_time
 
-    def __str__(self):
-        return self.lead_name
+    def sav_user(self, user):
+        user.save()
 
-    def __str__(self):
-        return self.first_call_time
+    def sav_user(self, name):
+        name.save()
 
-    def __str__(self):
-        return self.total_talk_time
+    def sav_user(self, date):
+        date.save()
+
+    def sav_user(self, first_call_time):
+        first_call_time.save()
+
+    def sav_user(self, total_talk_time):
+        total_talk_time.save()
+
+    
