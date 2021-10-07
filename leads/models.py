bfe84ef5f778b177860c687717db12cee3e6a1cd
Scott Wright <scottwright.qrf@gmail.com>
2021-10-07 08:51:01 -0500

    corrections to lead_repository and started creating email data service

--- leads/models.py
+++ leads/models.py
@@ -1,10 +1,6 @@
 from django.db import models
 
-# TODO: 
-# rename to Lead
-# add display_name field (string)
-# make first_call_time and total_talk_time strings for simplicity
-# rename date to created_date
+
 class Lead(models.Model):
     user_name = models.CharField(max_length=100)
     display_name = models.CharField(max_length=100)
--- leads/services/lead_repository.py
+++ leads/services/lead_repository.py
@@ -8,23 +8,16 @@ class LeadRepository:
     @staticmethod
     def save_leads(scraped_leads: list[ScrapedLead]):
         for scraped_lead in scraped_leads:
-            # for l in lead:
-            user_name = scraped_lead.user # swap these
+            
+            user_name = scraped_lead.user 
             display_name = scraped_lead.name
             first_call_time = scraped_lead.first_call_time
             total_talk_time = scraped_lead.total_talk_time
-            created_date = parse(scraped_lead.date) # parse this
+            created_date = parse(scraped_lead.date)
 
             Lead.objects.create(
                 user_name=user_name, 
                 display_name=display_name, 
                 first_call_time=first_call_time, 
                 total_talk_time=total_talk_time, 
-                created_date=created_date)
-
-            # LeadRepository.save_leads.user_name.save()
-            # LeadRepository.save_leads.display_name.save()
-            # LeadRepository.save_leads.first_call_time.save()
-            # LeadRepository.save_leads.total_talk_time.save()
-            # LeadRepository.save_leads.created_date.save()
-    
+                created_date=created_date,)
