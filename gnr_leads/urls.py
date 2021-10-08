293087b5e59e1f67469a2c0d8bdea104eadbd39c
Scott Wright <scottwright.qrf@gmail.com>
2021-10-07 22:05:19 -0500

    updates

--- gnr_leads/urls.py
+++ gnr_leads/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('leads.urls'))
+    path('', include('leads.urls')),
+    path('email_view/', include('leads.urls')),
+
 ]
--- leads/urls.py
+++ leads/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+
+from leads.services.email_data import Email
 from . import views
 
 app_name = 'leads'
 urlpatterns = [
-  path('', views.index, name='index')
+  path('', views.index, name='index'),
+  path('/email_view/', views.email_view, name = 'email_view')
 ]
