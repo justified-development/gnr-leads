05a09d58ad2279c1dc62238f70896c8b88e431ae
Brandon Wright <brandon.wright@ticketmaster.com>
2021-10-26 15:14:06 -0500

    Tweaked date on template

--- scripts/email_test.py
+++ scripts/email_test.py
@@ -1,7 +1,8 @@
 from leads.services.email_sender import EmailSender
 from django.utils import timezone
+from datetime import datetime
 
 def run(*args):
   date = '2021-10-26'
-  EmailSender.send_email(date)
-  
+#   EmailSender.send_email(datetime.today())
+EmailSender.send_email(date)
