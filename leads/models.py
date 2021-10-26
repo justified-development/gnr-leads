1d071e73a4ecdb491d61436559e2b5f075e0c2b3
Brandon Wright <brandon.wright@ticketmaster.com>
2021-10-26 12:16:37 -0500

    Extend two field lengths

--- leads/models.py
+++ leads/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Lead(models.Model):
     user_name = models.CharField(max_length=100)
     display_name = models.CharField(max_length=100)
-    first_call_time = models.CharField(max_length= 5)
-    total_talk_time = models.CharField(max_length= 5)
+    first_call_time = models.CharField(max_length= 20)
+    total_talk_time = models.CharField(max_length= 20)
     created_date = models.DateTimeField()
 
 
