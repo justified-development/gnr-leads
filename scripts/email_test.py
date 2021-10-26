a3e344447dfcedf3f395251a555cd9d57098f809
Scott Wright <scottwright.qrf@gmail.com>
2021-10-25 22:35:42 -0500

    updates

--- scripts/email_test.py
+++ scripts/email_test.py
@@ -1,6 +1,7 @@
 from leads.services.email_data import EmailSender
+from django.utils import timezone
 
 def run(*args):
-  date = '2021-10-02'
+  date = '2021-10-25'
   EmailSender.send_email(date)
   # this is just an example
