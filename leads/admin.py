7a09048db322c49f63c752c3697a94ba7cb7b762
Brandon Wright <brandon.wright@ticketmaster.com>
2021-10-26 13:59:13 -0500

    Update admin

--- leads/admin.py
+++ leads/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Lead
+from .models import Lead, Account, Message
 
 # Register your models here.
 admin.site.register(Lead)
+admin.site.register(Account)
+admin.site.register(Message)
