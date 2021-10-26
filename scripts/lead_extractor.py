f458c7abd48e8c6d6b37d0dfe4f66cfceaf24965
Brandon Wright <brandon.wright@ticketmaster.com>
2021-10-26 12:07:22 -0500

    Update extract date (temp)

--- scripts/lead_extractor.py
+++ scripts/lead_extractor.py
@@ -2,7 +2,7 @@ from leads.services.HAScraper import HAScraper, ScrapedLead
 from leads.services.lead_repository import LeadRepository
 
 def run(*args):
-  date = '10/2/2021'
+  date = '10/26/2021'
   leads: list[ScrapedLead] = HAScraper.scrape(date)
   print(leads)
   # TODO: nothing to do here, just note that this is how we will use your new class
