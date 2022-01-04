79a352bc14083b52628b7e29c43f90d052af58d7
Brandon Wright <brandon.wright@ticketmaster.com>
2022-01-04 09:43:22 -0600

    fix date format

--- scripts/lead_extractor.py
+++ scripts/lead_extractor.py
@@ -5,7 +5,7 @@ from leads.services.email_sender import EmailSender
 
 def run(*args):
   yesterday = datetime.today() - timedelta(days=1)
-  date_string = yesterday.strftime('%m/%-d/%Y')
+  date_string = yesterday.strftime('%-m/%-d/%Y')
   leads: list[ScrapedLead] = Scraper.scrape(date_string)
   print(leads)
   LeadRepository.save_leads(leads)
