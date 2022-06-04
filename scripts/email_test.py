f00241098e72ca4df1178295316742a761c6a9ac
Brandon Wright <bwright2810@gmail.com>
2022-06-04 11:36:39 -0500

    fix email test

--- scripts/email_test.py
+++ scripts/email_test.py
@@ -3,6 +3,6 @@ from django.utils import timezone
 from datetime import datetime
 
 def run(*args):
-  date = '2021-10-26'
-#   EmailSender.send_email(datetime.today())
-EmailSender.send_email(date)
+  # date = '2021-10-26'
+  EmailSender.send_email(datetime.today())
+# EmailSender.send_email(date)
