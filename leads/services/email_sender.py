21b95ef8e45b30247bb24c1c5d142d037ceeb741
Brandon Wright <bwright2810@gmail.com>
2022-12-03 22:27:38 -0600

    set variables back

--- leads/services/email_sender.py
+++ leads/services/email_sender.py
@@ -7,8 +7,8 @@ from leads.models import Message
 class EmailSender:
 
     @staticmethod
-    def send_email(dates):
-        leads = Lead.objects.filter(run_date__in=list(dates)).order_by('account_name', 'initial_lead_date_time', 'won_job_date_time')
+    def send_email(date):
+        leads = Lead.objects.filter(run_date = date).order_by('account_name', 'initial_lead_date_time', 'won_job_date_time')
         # print(leads)
         sender = settings.EMAIL_HOST_USER
         receiver = settings.EMAIL_RECIPIENT
@@ -21,12 +21,12 @@ class EmailSender:
             print("Warning: multiple Message records. Just using the first.")
         message = messages[0]
 
-        rendered_content = render_to_string('email.html', { "leads": leads, "prelude": message.prelude, "today": dates[0]})
+        rendered_content = render_to_string('email.html', { "leads": leads, "prelude": message.prelude, "today": date})
         print("Recipients: " + str(recs))
         # print("Content: " + rendered_content)
 
         send_mail(
-            message.title + '(Last 4 Days)',
+            message.title,
             '', 
             sender,
             recs,
--- scripts/lead_extractor.py
+++ scripts/lead_extractor.py
@@ -6,14 +6,8 @@ from leads.services.email_sender import EmailSender
 def run(*args):
   yesterday = datetime.today() - timedelta(days=1)
   date_string = yesterday.strftime('%-m/%-d/%Y')
-  yesterday2 = datetime.today() - timedelta(days=2)
-  date_string2 = yesterday2.strftime('%-m/%-d/%Y')
-  yesterday3 = datetime.today() - timedelta(days=3)
-  date_string3 = yesterday3.strftime('%-m/%-d/%Y')
-  yesterday4 = datetime.today() - timedelta(days=4)
-  date_string4 = yesterday4.strftime('%-m/%-d/%Y')
-  # leads: list[ScrapedLead] = Scraper.scrape([date_string, date_string2, date_string3, date_string4])
-  # print(leads)
-  # LeadRepository.save_leads(leads)
-  EmailSender.send_email([yesterday, yesterday2, yesterday3, yesterday4])
+  leads: list[ScrapedLead] = Scraper.scrape(date_string)
+  print(leads)
+  LeadRepository.save_leads(leads)
+  EmailSender.send_email(yesterday)
   
