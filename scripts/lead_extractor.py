3b5b91ecee0a7fc93c61a87cf1f9717801b96083
Brandon Wright <brandon.wright@ticketmaster.com>
2022-12-08 08:31:41 -0600

    add ability to skip scrape

--- scripts/lead_extractor.py
+++ scripts/lead_extractor.py
@@ -2,12 +2,14 @@ from leads.services.scraper import Scraper, ScrapedLead
 from leads.services.lead_repository import LeadRepository
 from datetime import datetime, timedelta
 from leads.services.email_sender import EmailSender
+from django.conf import settings
 
 def run(*args):
   yesterday = datetime.today() - timedelta(days=1)
   date_string = yesterday.strftime('%-m/%-d/%Y')
-  leads: list[ScrapedLead] = Scraper.scrape(date_string)
-  print(leads)
-  LeadRepository.save_leads(leads)
+  if not settings.SKIP_SCRAPE == "true":
+    leads: list[ScrapedLead] = Scraper.scrape(date_string)
+    print(leads)
+    LeadRepository.save_leads(leads)
   EmailSender.send_email(yesterday)
   
