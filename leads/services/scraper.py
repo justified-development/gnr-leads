0b8e5cd268495da6325c11f31c2825167d6c233a
Brandon Wright <brandon.wright@ticketmaster.com>
2024-08-28 13:18:16 -0400

    print6

--- leads/services/scraper.py
+++ leads/services/scraper.py
@@ -48,6 +48,7 @@ class Scraper:
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument("--enable-javascript")
     chrome_options.binary_location = Scraper.GOOGLE_CHROME_PATH
 
     driver = webdriver.Chrome(executable_path=Scraper.CHROMEDRIVER_PATH, chrome_options=chrome_options)
